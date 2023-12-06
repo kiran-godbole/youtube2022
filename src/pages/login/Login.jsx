cdb7a1fed0492ea6360aed87b2faa6f8ff6041d8
Kiran Godbole <kiran.pgodbole@gmail.com>
2023-12-06 11:56:53 +0530

    login using firebase auth

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,43 @@
-import "./login.scss"
+import { useState } from "react"
+import "./login.scss";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../../firebase'
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [error, setError] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        console.log(user)
+        navigate("/")
+      })
+      .catch((error) => {
+        setError(true)
+      });
+  }
   return (
-    <div>Login</div>
+    <div className="login">
+      <form onSubmit={handleLogin}>
+        <input type="email" placeholder="email"
+          onChange={e => setEmail(e.target.value)}
+        />
+        <input type="password" placeholder="password"
+          onChange={e => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+        {error && <span>Wrong email and password</span>}
+      </form>
+    </div>
   )
 }
 
